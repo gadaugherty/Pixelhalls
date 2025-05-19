22eb595512a523fe7d362d79338656d8c26ce2af
gadaugherty <daugherty.exe@gmail.com>
2025-05-18 23:52:17 -0700

    Update spider.js

--- spider/spider.js
+++ spider/spider.js
@@ -1,6 +1,6 @@
 // spider.js (Clean Pixel Grid Flashing Green)
 
-const canvas = document.getElementById("spiderCanvas");
+
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
